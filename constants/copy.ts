a6ce968054b2c21bd974731e31766b2262449656
Marco Goedert <maarcoagg@gmail.com>
2026-03-18 12:36:51 -0300

    editorial note: concise, evergreen

--- constants/copy.ts
+++ constants/copy.ts
@@ -1,1 +1,1 @@
-export const EDITORIAL_NOTE = "Platform engineer at TELUS Digital. Been shipping software professionally since 2019.";
+export const EDITORIAL_NOTE = "Software engineer. Updated occasionally.";
